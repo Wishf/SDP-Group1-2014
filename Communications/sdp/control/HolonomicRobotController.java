999977fd8d1380d0ac4899e00b6eeac3fcbe9d92
Matthew Summers <matt@wishf.co.uk>
2015-02-09 15:04:49 +0000

    Collect some constants

--- Communications/sdp/control/HolonomicRobotController.java
+++ Communications/sdp/control/HolonomicRobotController.java
@@ -12,6 +12,16 @@ public class HolonomicRobotController extends BaseRobotController {
     // http://www.microrobo.com/58mm-lego-compatible-omni-wheel.html
     private static final int WHEEL_DIAMETER = 58;
     private static final double WHEEL_CIRCUMFERENCE = Math.PI * WHEEL_DIAMETER;
+    private static final double WHEEL_DEG_PER_DIST = (double)(360 / WHEEL_CIRCUMFERENCE);
+    // Distances from center of wheel to center of axle (back wheel measured from middle of front axle; probably wrong)
+    private static final double WHEEL_LEFT_DIST = 57.5;
+    private static final double WHEEL_RIGHT_DIST = 57.5;
+    private static final double WHEEL_BACK_DIST = 80;
+
+    private static final int GROUP9_WHEEL_DIAMETER = 57;
+    private static final double GROUP9_WHEEL_CIRCUMFERENCE = Math.PI * GROUP9_WHEEL_DIAMETER;
+
+    private static final double WHEEL_RATIO = GROUP9_WHEEL_DIAMETER / (double)WHEEL_DIAMETER;
 
     public HolonomicRobotController(Radio radio,
                                     boolean initialCatcher,
